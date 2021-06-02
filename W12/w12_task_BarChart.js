f61dbc8cd116dbde90b782ee0b7bed30e223778c
unknown <Okada Eisuke>
2021-06-02 15:30:10 +0900

    Initial commit

--- W12/w12_task_BarChart.js
+++ W12/w12_task_BarChart.js
@@ -27,20 +27,22 @@ class BarChart {
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
         self.xscale = d3.scaleBand()
-            .range([0, self.inner_width])
-            .paddingInner(0.2)
+            .domain(self.data.map(d => d.day))
+            .rangeRound([0, self.inner_width])
+            .paddingInner(0.5)
             .paddingOuter(0.1);
 
         self.yscale = d3.scaleLinear()
-            .domain([0,5000])
-            .range([self.inner_height, 0]);
+            .domain([0, d3.max(self.data, d => d.number)])
+            // .domain([0,7000])
+            .range([self.inner_height,0]);
 
         self.xaxis = d3.axisBottom(self.xscale)
-            .ticks(['setosa','versicolor','virginica'])
+            // .ticks()
             .tickSizeOuter(0);
 
         self.yaxis = d3.axisLeft(self.yscale)
-            .ticks(5)
+            // .ticks(5)
             .tickSizeOuter(0);
 
         self.xaxis_group = self.chart.append('g')
@@ -51,8 +53,10 @@ class BarChart {
         const xlabel_space = 40;
         self.svg.append('text')
             .style('font-size', '12px')
+            // .attr('transform', `rotate(-90)`)
             .attr('x', self.config.width / 2)
             .attr('y', self.inner_height + self.config.margin.top + xlabel_space)
+            // .attr('writing-mode', 'tb')
             .text( self.config.xlabel );
 
         const ylabel_space = 50;
@@ -63,26 +67,32 @@ class BarChart {
             .attr('x', -(self.config.height / 2))
             .attr('text-anchor', 'middle')
             .attr('dy', '1em')
-            .attr('writing-mode', 'tb')
+            // .attr('writing-mode', 'tb')
             .text( self.config.ylabel );
     }
 
     update() {
         let self = this;
 
-        const data_map = d3.rollup( self.data, v => v.length, d => d.day );
-        self.aggregated_data = Array.from( data_map, ([key,count]) => ({key,count}) );
+        // const data_map = d3.rollup( self.data, v => v.length, d => d.day );
+        // self.aggregated_data = Array.from( data_map, ([key,count]) => ({key,count}) );
 
-        self.cvalue = d => d.key;
-        self.xvalue = d => d.key;
-        self.yvalue = d => d.count;
+        // self.cvalue = d => d.key;
+        // self.xvalue = d => d.key;
+        // self.yvalue = d => d.count;
 
-        const items = self.aggregated_data.map( self.xvalue );
-        self.xscale.domain(items);
+        // const items = self.aggregated_data.map( self.xvalue );
+        // self.xscale.domain(items);
 
-        const ymin = 0;
-        const ymax = d3.max( self.aggregated_data, self.yvalue );
-        self.yscale.domain([ymin, ymax]);
+        // const ymin = 0;
+        // const ymax = d3.max( self.aggregated_data, self.yvalue );
+        // self.yscale.domain([ymin, ymax]);
+        // self.yscale.domain([0, d3.max(self.data, d => d.number)])
+
+
+
+        // const ymin = d3.min( self.data, d => d.number );
+        // const ymax = d3.max( self.data, d => d.number );
 
         self.render();
     }
@@ -90,30 +100,40 @@ class BarChart {
     render() {
         let self = this;
 
-        self.chart.selectAll(".bar")
-            .data(self.aggregated_data)
-            .join("rect")
-            .attr("class", "bar")
-            .attr("x", d => self.xscale( self.xvalue(d) ) )
-            .attr("y", d => self.yscale( self.yvalue(d) ) )
+        self.chart.selectAll("rect")
+            .data(self.data)
+            // .join("rect")
+            // .attr("class", "bar")
+            // .attr("x", d => self.xscale( self.xvalue(d) ) )
+            .enter()
+            .append("rect")
+            .attr("x", d => self.xscale(d.day))
+            .attr("y", d => self.yscale(d.number))
+            // .attr("y",0)
             .attr("width", self.xscale.bandwidth())
-            .attr("height", d => self.inner_height - self.yscale( self.yvalue(d) ))
-            .attr("fill", d => self.config.cscale( self.cvalue(d) ))
-            .on('click', function(ev,d) {
-                const is_active = filter.includes(d.key);
-                if ( is_active ) {
-                    filter = filter.filter( f => f !== d.key );
-                }
-                else {
-                    filter.push( d.key );
-                }
-                Filter();
-                d3.select(this).classed('active', !is_active);
-            });
+            // .attr("height", d => self.inner_height - self.yscale( self.yvalue(d) ))
+            .attr("height",d => self.inner_height-self.yscale(d.number))
+            // .attr("fill", d => self.config.cscale( self.cvalue(d) ))
+            .attr("fill","black")
+            // .on('click', function(ev,d) {
+            //     const is_active = filter.includes(d.key);
+            //     if ( is_active ) {
+            //         filter = filter.filter( f => f !== d.key );
+            //     }
+            //     else {
+            //         filter.push( d.key );
+            //     }
+            //     Filter();
+            //     d3.select(this).classed('active', !is_active);
+            // });
+
+        
 
         self.xaxis_group
-            .call(self.xaxis);
-
+            .call(self.xaxis)
+            .selectAll("text")
+            .attr("transform","rotate(60)")
+            .attr("text-anchor","start")
         self.yaxis_group
             .call(self.yaxis);
     }
--- W12/w12_task_main.js
+++ W12/w12_task_main.js
@@ -7,18 +7,18 @@ d3.csv("https://okadaeisuke.github.io/infovis2021/W12/pcr_positive_daily.csv")
     .then( data => {
         input_data = data;
         input_data.forEach( d => {
-            d.sepal_length = +d.sepal_length;
-            d.sepal_width = +d.sepal_width;
+            d.number = +d.number;
+            // d.sepal_width = +d.sepal_width;
         });
 
         const color_scale = d3.scaleOrdinal( d3.schemeCategory10 );
-        color_scale.domain(['setosa','versicolor','virginica']);
+        // color_scale.domain(['setosa','versicolor','virginica']);
 
         scatter_plot = new ScatterPlot( {
             parent: '#drawing_region_scatterplot',
-            width: 1024,
-            height: 256,
-            margin: {top:10, right:10, bottom:50, left:50},
+            width: 512,
+            height: 512,
+            margin: {top:10, right:10, bottom:100, left:50},
             xlabel: 'Sepal length [cm]',
             ylabel: 'Sepal width [cm]',
             cscale: color_scale
@@ -27,9 +27,9 @@ d3.csv("https://okadaeisuke.github.io/infovis2021/W12/pcr_positive_daily.csv")
 
         bar_chart = new BarChart( {
             parent: '#drawing_region_barchart',
-            width: 1024,
-            height: 1024,
-            margin: {top:10, right:10, bottom:50, left:50},
+            width: 512,
+            height: 512,
+            margin: {top:10, right:10, bottom:100, left:50},
             xlabel: 'Day',
             cscale: color_scale
         }, input_data );
